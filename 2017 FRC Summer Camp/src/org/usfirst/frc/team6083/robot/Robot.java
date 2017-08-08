6135756ed4cc5725fa7c69178d89b39762568789
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-08-08 17:53:27 +0800

    Version One

--- 2017 FRC Summer Camp/src/org/usfirst/frc/team6083/robot/Robot.java
+++ 2017 FRC Summer Camp/src/org/usfirst/frc/team6083/robot/Robot.java
@@ -1,8 +1,15 @@
 package org.usfirst.frc.team6083.robot;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -12,69 +19,60 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	VictorSP leftmotor = new VictorSP(0);
+	VictorSP rightmotor = new VictorSP(1);
+	VictorSP encodermotor = new VictorSP(2);
+	Joystick stick = new Joystick(0);
+	
+	Encoder encoder = new Encoder(0,1, false, Encoder.EncodingType.k4X);
+	ADXRS450_Gyro Gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+	PIDController pid = new PIDController(0.02, 0, 0, encoder, encodermotor);
+	
+	public static double speedy = 1.0, slow = 0.6;
+	public static double direct = 1.0;
+	
+	RobotDrive myRobot = new RobotDrive(leftmotor,rightmotor);
+	
 	@Override
 	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
+		CameraServer.getInstance().startAutomaticCapture(0);
+		CameraServer.getInstance().startAutomaticCapture(1);
 	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
+	
 	@Override
-	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+	public void teleopInit(){
+		pid.enable();
 	}
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
-	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
+	
 	@Override
 	public void teleopPeriodic() {
-	}
+		if (stick.getRawButton(11)) {
+			direct =-1.0;
+		}
+		else{
+			direct =1.0;
+		}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
+		if (stick.getRawButton(1)) {
+			myRobot.arcadeDrive(stick.getY() *(-direct*speedy), stick.getX() *(-direct*speedy));
+		} 
+		else {
+			myRobot.arcadeDrive(stick.getY() *(-direct*slow), stick.getX() *(-direct*slow));
+		}
+
+		if (stick.getRawButton(2)) {
+			while (!stick.getRawButton(3)) {
+				pid.setSetpoint((stick.getThrottle()+1)*30.00);
+				if (stick.getRawButton(1)) {
+					myRobot.arcadeDrive(stick.getY() *(-direct*speedy), stick.getX() *(-direct*speedy));
+				}
+				else {
+					myRobot.arcadeDrive(stick.getY() *(-direct*slow), stick.getX() *(-direct*slow));
+				}
+			}
+			pid.setSetpoint(0);
+		}
+		Timer.delay(0.005);	 
 	}
 }
 
