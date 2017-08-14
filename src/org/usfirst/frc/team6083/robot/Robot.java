369869b5604ba993abee232e4ab60a147c83bbf5
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2017-08-14 22:30:10 +0800

    Add comment

--- src/org/usfirst/frc/team6083/robot/Robot.java
+++ src/org/usfirst/frc/team6083/robot/Robot.java
@@ -1,13 +1,11 @@
 package org.usfirst.frc.team6083.robot;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
 
@@ -22,28 +20,46 @@ public class Robot extends IterativeRobot {
 	VictorSP leftmotor = new VictorSP(0);
 	VictorSP rightmotor = new VictorSP(1);
 	VictorSP encodermotor = new VictorSP(2);
+	//define motors
+	
 	Joystick stick = new Joystick(0);
+	//define Joystick
 	
 	Encoder encoder = new Encoder(0,1, false, Encoder.EncodingType.k4X);
-	ADXRS450_Gyro Gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+	//define encoder
+	
 	PIDController pid = new PIDController(0.02, 0, 0, encoder, encodermotor);
+	//define PIDController
 	
 	public static double speedy = 1.0, slow = 0.6;
 	public static double direct = 1.0;
+	//define variable
 	
 	RobotDrive myRobot = new RobotDrive(leftmotor,rightmotor);
+	//define robot drive
 	
+	/*
+	 * This method will called one time during robot start.
+	 */
 	@Override
 	public void robotInit() {
 		CameraServer.getInstance().startAutomaticCapture(0);
 		CameraServer.getInstance().startAutomaticCapture(1);
+		//start USB cameras
 	}
 	
+	/*
+	 * This method will called one time during teleop mode start.
+	 */
 	@Override
 	public void teleopInit(){
 		pid.enable();
+		//Begin running the PIDController
 	}
 	
+	/*
+	 * This method will called periodic during teleop mode.
+	 */
 	@Override
 	public void teleopPeriodic() {
 		if (stick.getRawButton(11)) {
@@ -73,6 +89,7 @@ public class Robot extends IterativeRobot {
 			pid.setSetpoint(0);
 		}
 		Timer.delay(0.005);	 
+		//delay for 5ms
 	}
 }
 
